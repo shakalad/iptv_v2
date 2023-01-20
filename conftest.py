c3f9753bdb0b0d1cfa65aa78c7b6a9ba3119f78d
Arthur <shakalad92@gmail.com>
2023-01-20 17:39:10 +0400

    added fake_useragent

--- conftest.py
+++ conftest.py
@@ -1,21 +1,20 @@
-import os
-
 import pytest
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from fake_useragent import FakeUserAgent
+
 
 @pytest.fixture()
 def browser_driver():
     driver_options = webdriver.ChromeOptions()
-    preferences = {"download.default_directory": os.getcwd() + "/downloads"}
-    driver_options.add_experimental_option("prefs", preferences)
     driver_options.add_argument('--window-size=1920,1080')
-    # driver_options.add_argument("--no-sandbox")
-    # driver_options.headless = True
+    driver_options.add_argument(f'user-agent={FakeUserAgent().random}')
+    driver_options.add_argument("--no-sandbox")
+    driver_options.headless = True
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=driver_options)
-    driver.maximize_window()
+    # driver.maximize_window()
     yield driver
     driver.quit()
