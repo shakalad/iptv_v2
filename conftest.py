9e996d4c7a44871f4378e0dda97b6e892a30fbba
Arthur <shakalad92@gmail.com>
2023-01-20 17:25:02 +0400

    Trying to execute script without headless

--- conftest.py
+++ conftest.py
@@ -13,9 +13,9 @@ def browser_driver():
     preferences = {"download.default_directory": os.getcwd() + "/downloads"}
     driver_options.add_experimental_option("prefs", preferences)
     driver_options.add_argument('--window-size=1920,1080')
-    driver_options.add_argument("--no-sandbox")
-    driver_options.headless = True
+    # driver_options.add_argument("--no-sandbox")
+    # driver_options.headless = True
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=driver_options)
-    # driver.maximize_window()
+    driver.maximize_window()
     yield driver
     driver.quit()
