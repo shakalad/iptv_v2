2f589c6b3a3724d15a0f6debc885e71766404714
Arthur <shakalad92@gmail.com>
2023-01-23 14:35:08 +0400

    Commented fake user agent and window size

--- conftest.py
+++ conftest.py
@@ -10,8 +10,8 @@ from fake_useragent import FakeUserAgent
 @pytest.fixture()
 def browser_driver():
     driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--window-size=1920,1080')
-    driver_options.add_argument(f"user-agent={FakeUserAgent().chrome}")
+    # driver_options.add_argument('--window-size=1920,1080')
+    # driver_options.add_argument(f"user-agent={FakeUserAgent().chrome}")
     # driver_options.add_argument("--no-sandbox")
     # driver_options.headless = True
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=driver_options)
