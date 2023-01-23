c5d3b82114cb034a81e4d423ae32aca93e3b436b
Arthur <shakalad92@gmail.com>
2023-01-23 14:25:07 +0400

    Added login to register page befour logging in as administrator

--- tests/test_check_admin_balance.py
+++ tests/test_check_admin_balance.py
@@ -1,8 +1,12 @@
 from pages.vipdrive_pages.login_page import LoginPage
 from pages.vipdrive_pages.overview_page import OverviewPage
+from pages.vipdrive_pages.signup_page import SignUpPage
 
 
 def test_check_admin_balance(browser_driver):
+    # Open register page
+    signup_page = SignUpPage(browser_driver)
+
     # Login as Admin
     login_page = LoginPage(browser_driver)
     login_page.login_as_admin()
