d87dc40c3e4247d3b35d991c2bcfb86d82506d04
Arthur <shakalad92@gmail.com>
2023-01-26 01:02:08 +0400

    sssss

--- pages/vipdrive_pages/login_page.py
+++ pages/vipdrive_pages/login_page.py
@@ -23,9 +23,13 @@ class LoginPage(BasePage):
         self.email.set_text("shakalad92@gmail.com")
         self.password.set_text("otxqfsw2u")
         self.submit_btn.click_button()
-        time.sleep(30)
-        self.driver.execute_script(f"document.getElementById('g-recaptcha-response').style.display = 'initial';")
-        time.sleep(20)
+        while self.driver.current_url == self.url:
+            self.driver.refresh()
+            time.sleep(5)
+            self.driver.execute_script(f"document.getElementById('g-recaptcha-response').style.display = 'initial';")
+            self.driver.execute_script(
+                f"document.getElementById('g-recaptcha-response').innerHTML='{'sexyyyyyyyyyyyyyy'}';")
+            self.submit_btn.click_button()
         # if self.driver.current_url == self.url:
         #     print("SEXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
         #     self.email.set_text("shakalad92@gmail.com")
