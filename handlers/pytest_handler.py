02c95c50472a72cf620436798f12b420e3fc2eb3
Arthur <arthur.petrosyan@stenn.com>
2023-12-31 15:12:24 +0400

    Implemented user activation method

--- handlers/pytest_handler.py
+++ handlers/pytest_handler.py
@@ -9,6 +9,7 @@ class PytestHandler:
         self.___create_command = create
         self.___recharge_command = recharge
         self.___check_command = check
+        self.__activate_command = activate
 
     def __execute(self, command):
         if subprocess.call(command, shell=True, stderr=subprocess.PIPE) != 0:
@@ -22,3 +23,6 @@ class PytestHandler:
 
     def check_balance(self):
         self.__execute(self.___check_command)
+
+    def activate_user(self):
+        self.__execute(self.__activate_command)
