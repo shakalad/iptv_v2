8e75ab15113a01d5700737a63b44a8dbc8ad0163
Arthur <arthur.petrosyan@stenn.com>
2024-01-04 11:51:48 +0400

    Added User class and added necessary steps for recharg and activate

--- tests/test_recharge.py
+++ tests/test_recharge.py
@@ -1,5 +1,6 @@
 import time
 
+from handlers.user_handler import User
 from pages.vipdrive_pages.login_page import LoginPage
 from pages.vipdrive_pages.money_transfer_page import MoneyTransferPage
 from pages.vipdrive_pages.overview_page import OverviewPage
@@ -21,6 +22,14 @@ def test_recharge(browser_driver):
     email = json_handler.read_file()['email']
     amount = json_handler.read_file()['amount']
     money_transfer_page.transfer_money_to_the_user(email, amount)
+
+    # Logout from admin
+    money_transfer_page.logout()
+
+    # Login as user
+    user_credentials = User(email=email, password="904070")
+    login_page = LoginPage(browser_driver)
+    login_page.login_as_user(user_credentials)
 
     # Activate tariff
     tariff_page = TariffPage(browser_driver)
